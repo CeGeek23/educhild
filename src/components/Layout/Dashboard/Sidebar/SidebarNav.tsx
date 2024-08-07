fec7d39e9c95a7f4724545906d94e139110435d9
cegeek23 <118506388+CeGeek23@users.noreply.github.com>
2024-08-08 00:28:34 +0200

    fonctionnalité de localisation

--- src/components/Layout/Dashboard/Sidebar/SidebarNav.tsx
+++ src/components/Layout/Dashboard/Sidebar/SidebarNav.tsx
@@ -34,17 +34,14 @@ export default async function SidebarNav() {
   const dict = await getDictionary()
   return (
     <ul className="list-unstyled">
+      <SidebarNavTitle>{dict.sidebar.items.administration}</SidebarNavTitle>
       <SidebarNavItem icon={faGauge} href="/" >
         {dict.sidebar.items.dashboard}
-        <small className="ms-auto"><Badge bg="info" className="ms-auto">NEW</Badge></small>
       </SidebarNavItem>
-      <SidebarNavItem icon={faCode} href="/pokemons">
-        {dict.sidebar.items.sample}
-        <small className="ms-auto"><Badge bg="danger" className="ms-auto">DEMO</Badge></small>
+      <SidebarNavItem icon={faFaceSmile} href="/pokemons">
+        My Family
       </SidebarNavItem>
 
-      <SidebarNavTitle>{dict.sidebar.items.administration}</SidebarNavTitle>
-      <SidebarNavItem icon={faFaceSmile} href="#">{dict.sidebar.items.family}</SidebarNavItem>
       <SidebarNavItem icon={faAndroid} href="#">{dict.sidebar.items.devices}</SidebarNavItem>
 
       <SidebarNavTitle>{dict.sidebar.items.extras}</SidebarNavTitle>
@@ -52,16 +49,13 @@ export default async function SidebarNav() {
         <SidebarNavItem icon={faBug} href="report_site">{dict.sidebar.items.report_website}</SidebarNavItem>
         <SidebarNavItem icon={faBug} href="report_app">{dict.sidebar.items.report_application}</SidebarNavItem>
       </SidebarNavGroup>
-
-      <SidebarNavTitle>{dict.sidebar.items.theme}</SidebarNavTitle>
-      <SidebarNavItem icon={faLocationArrow} href="#">Localiser</SidebarNavItem>
+      <SidebarNavItem icon={faLocationArrow} href="/location">Locate your children</SidebarNavItem>
 
       <SidebarNavGroup toggleIcon={faBell} toggleText={dict.sidebar.items.notifications}>
         <SidebarNavItem href="#">{dict.sidebar.items.alerts}</SidebarNavItem>
         <SidebarNavItem href="#">{dict.sidebar.items.badge}</SidebarNavItem>
       </SidebarNavGroup>
 
-      <SidebarNavItem icon={faFileLines} href="/plan_international">About Plan International</SidebarNavItem>
     </ul>
   )
 }
